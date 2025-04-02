bb28af8e251eabdda70bb06a2440fbc1e628adb9
gt-yordanos <gt.yordanos@gmail.com>
2025-04-02 18:43:04 +0300

    added all of the routes in to the index.js

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,31 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 
+// Import route files
+import adminRoutes from './routes/admin.js';
+import customerRoutes from './routes/customer.js';
+import deliveryRoutes from './routes/delivery.js';
+import deliveryPersonRoutes from './routes/deliveryPerson.js';
+import menuRoutes from './routes/menu.js';
+import orderRoutes from './routes/order.js';
+import restaurantRoutes from './routes/restaurant.js';
+import restaurantOwnerRoutes from './routes/restaurantOwner.js';
+
 dotenv.config();
 
 const app = express();
 connectDB();
 app.use(express.json());
+
+// Register routes
+app.use('/api/admin', adminRoutes);
+app.use('/api/customers', customerRoutes);
+app.use('/api/delivery', deliveryRoutes);
+app.use('/api/delivery-persons', deliveryPersonRoutes);
+app.use('/api/menu', menuRoutes);
+app.use('/api/orders', orderRoutes);
+app.use('/api/restaurants', restaurantRoutes);
+app.use('/api/restaurant-owners', restaurantOwnerRoutes);
 
 // Default route
 app.get('/', (req, res) => {
