1e7580d9cdbf977f9d8a925321788cf2898f4902
gt-yordanos <gt.yordanos@gmail.com>
2025-04-03 16:07:27 +0300

    fixed toggling issue of the theme toggler

--- frontend/src/components/shared/ThemeToggle.jsx
+++ frontend/src/components/shared/ThemeToggle.jsx
@@ -5,18 +5,15 @@ const ThemeToggle = () => {
 
   return (
     <label className="swap swap-rotate">
-      {/* This hidden checkbox controls the state */}
-      <input
-        type="checkbox"
-        className="theme-controller"
-        value="synthwave"
-        checked={theme === 'black'} // Reflect the theme
-        onChange={toggleTheme} // Toggle between light and dark
+      <input 
+        type="checkbox" 
+        className="theme-controller" 
+        checked={theme === 'halloween'}
+        onChange={toggleTheme}
       />
 
-      {/* Sun icon (when dark theme is off) */}
       <svg
-        className="swap-off h-10 w-10 fill-current"
+        className="swap-off h-8 w-8 fill-current"
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
       >
@@ -25,14 +22,13 @@ const ThemeToggle = () => {
         />
       </svg>
 
-      {/* Moon icon (when dark theme is on) */}
       <svg
-        className="swap-on h-10 w-10 fill-current"
+        className="swap-on h-8 w-8 fill-current"
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
       >
         <path
-          d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"
+          d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z"
         />
       </svg>
     </label>
