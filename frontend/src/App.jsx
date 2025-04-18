fc08f1e05fbd7e58489dce627840ad094c36978a
gt-yordanos <gt.yordanos@gmail.com>
2025-04-19 01:15:20 +0300

    organized imported pages and components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,18 +4,16 @@ import { AuthProvider } from './contexts/AuthContext';
 import ProtectedRoute from './components/shared/ProtectedRoute';
 
 import AdminLayout from './pages/admin/AdminLayout';
-import Dashboard from './pages/admin/Dashboard';
-import Customers from './pages/admin/Customers';
-import Owners from './pages/admin/Owners';
-import DeliveryPerson from './pages/shared/DeliveryPerson';
-import Restaurant from './pages/shared/Restaurant';
+import AdminDashboard from './pages/admin/Dashboard';
+import ManageCustomers from './pages/admin/Customers';
+import ManageOwners from './pages/admin/Owners';
+import ManageDeliveryPerson from './pages/shared/DeliveryPerson';
+import ManageRestaurant from './pages/shared/Restaurant';
 
 import OwnerLayout from './pages/owner/OwnerLayout';
 import OwnerDashboard from './pages/owner/Dashboard';
-import OwnerMenu from './pages/owner/Menu';
-import OwnerRestaurant from './pages/owner/Restaurant';
-import OwnerDeliveryPeople from './pages/owner/DeliveryPeople';
-import OwnerDelivery from './pages/owner/Delivery';
+import ManageMenu from './pages/owner/Menu';
+import ManageDelivery from './pages/owner/Delivery';
 
 import Login from './components/shared/Login';
 import api from './api';
@@ -35,19 +33,19 @@ const App = () => {
           <Route path="/" element={<ProtectedRoute requiredRole="customer"><p>Customer</p></ProtectedRoute>} />
 
           {/* Admin Routes */}
-          <Route path="/admin" element={<ProtectedRoute requiredRole="admin"><AdminLayout><Dashboard /></AdminLayout></ProtectedRoute>} />
-          <Route path="/admin/dashboard" element={<ProtectedRoute requiredRole="admin"><AdminLayout><Dashboard /></AdminLayout></ProtectedRoute>} />
-          <Route path="/admin/customers" element={<ProtectedRoute requiredRole="admin"><AdminLayout><Customers /></AdminLayout></ProtectedRoute>} />
-          <Route path="/admin/owners" element={<ProtectedRoute requiredRole="admin"><AdminLayout><Owners /></AdminLayout></ProtectedRoute>} />
-          <Route path="/admin/delivery-person" element={<ProtectedRoute requiredRole="admin"><AdminLayout><DeliveryPerson /></AdminLayout></ProtectedRoute>} />
-          <Route path="/admin/restaurant" element={<ProtectedRoute requiredRole="admin"><AdminLayout><Restaurant /></AdminLayout></ProtectedRoute>} />
+          <Route path="/admin" element={<ProtectedRoute requiredRole="admin"><AdminLayout><AdminDashboard /></AdminLayout></ProtectedRoute>} />
+          <Route path="/admin/dashboard" element={<ProtectedRoute requiredRole="admin"><AdminLayout><AdminDashboard /></AdminLayout></ProtectedRoute>} />
+          <Route path="/admin/customers" element={<ProtectedRoute requiredRole="admin"><AdminLayout><ManageCustomers /></AdminLayout></ProtectedRoute>} />
+          <Route path="/admin/owners" element={<ProtectedRoute requiredRole="admin"><AdminLayout><ManageOwners /></AdminLayout></ProtectedRoute>} />
+          <Route path="/admin/delivery-person" element={<ProtectedRoute requiredRole="admin"><AdminLayout><ManageDeliveryPerson /></AdminLayout></ProtectedRoute>} />
+          <Route path="/admin/restaurant" element={<ProtectedRoute requiredRole="admin"><AdminLayout><ManageRestaurant /></AdminLayout></ProtectedRoute>} />
 
           {/* Owner Routes */}
           <Route path="/owner/dashboard" element={<ProtectedRoute requiredRole="owner"><OwnerLayout><OwnerDashboard /></OwnerLayout></ProtectedRoute>} />
-          <Route path="/owner/menu" element={<ProtectedRoute requiredRole="owner"><OwnerLayout><OwnerMenu /></OwnerLayout></ProtectedRoute>} />
-          <Route path="/owner/restaurant" element={<ProtectedRoute requiredRole="owner"><OwnerLayout><OwnerRestaurant /></OwnerLayout></ProtectedRoute>} />
-          <Route path="/owner/delivery-people" element={<ProtectedRoute requiredRole="owner"><OwnerLayout><OwnerDeliveryPeople /></OwnerLayout></ProtectedRoute>} />
-          <Route path="/owner/delivery" element={<ProtectedRoute requiredRole="owner"><OwnerLayout><OwnerDelivery /></OwnerLayout></ProtectedRoute>} />
+          <Route path="/owner/menu" element={<ProtectedRoute requiredRole="owner"><OwnerLayout><ManageMenu /></OwnerLayout></ProtectedRoute>} />
+          <Route path="/owner/restaurant" element={<ProtectedRoute requiredRole="owner"><OwnerLayout><ManageRestaurant /></OwnerLayout></ProtectedRoute>} />
+          <Route path="/owner/delivery-people" element={<ProtectedRoute requiredRole="owner"><OwnerLayout><ManageDeliveryPeople /></OwnerLayout></ProtectedRoute>} />
+          <Route path="/owner/delivery" element={<ProtectedRoute requiredRole="owner"><OwnerLayout><ManageDelivery /></OwnerLayout></ProtectedRoute>} />
         </Routes>
       </Router>
     </AuthProvider>
