9a8f305e18fb4783ea7ad9e112632e44f8ccd79f
gt-yordanos <gt.yordanos@gmail.com>
2025-04-29 16:31:39 +0300

    created a restaurant context to fetch restaurant info from backend

---
+++ frontend/src/contexts/RestaurantContext.jsx
@@ -0,0 +1,37 @@
+import React, { createContext, useContext, useState, useEffect } from 'react';
+import axios from 'axios';
+import api from '../../api'; 
+// Define the context
+const RestaurantContext = createContext();
+
+// Define the RestaurantProvider component
+export const RestaurantProvider = ({ children }) => {
+  const [restaurant, setRestaurant] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    // Fetch the restaurant info when the component mounts
+    const fetchRestaurantInfo = async () => {
+      try {
+        const response = await axios.get(api.getAllRestaurantntInfo);
+        setRestaurant(response.data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchRestaurantInfo();
+  }, []);
+
+  return (
+    <RestaurantContext.Provider value={{ restaurant, loading, error }}>
+      {children}
+    </RestaurantContext.Provider>
+  );
+};
+
+// Custom hook to use the restaurant context
+export const useRestaurant = () => useContext(RestaurantContext);
