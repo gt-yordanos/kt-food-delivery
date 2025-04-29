23534c97a1a17f1abf90079f04624819b5bbc26e
gt-yordanos <gt.yordanos@gmail.com>
2025-04-29 16:36:11 +0300

    fixed a path calling issue

--- frontend/src/contexts/RestaurantContext.jsx
+++ frontend/src/contexts/RestaurantContext.jsx
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import axios from 'axios';
-import api from '../../api'; 
+import api from '../api.js'; 
 // Define the context
 const RestaurantContext = createContext();
 
