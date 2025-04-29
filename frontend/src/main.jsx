0f11df0cd094cc100e2cb79f500581bc171156c9
gt-yordanos <gt.yordanos@gmail.com>
2025-04-29 16:41:36 +0300

    add ed the retaurant provider inside the main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
 import { ThemeProvider } from './contexts/ThemeContext';
-import App from './App.jsx'
+import { RestaurantProvider } from './contexts/RestaurantContext';
+import App from './App.jsx';
 import { ToastContainer } from 'react-toastify';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider>
-      <ToastContainer />
-      <App />
+      <RestaurantProvider>
+        <ToastContainer />
+        <App />
+      </RestaurantProvider>
     </ThemeProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
