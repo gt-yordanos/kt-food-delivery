cf8d87e17471d455b3a318300c98b7403de6acd8
gt-yordanos <gt.yordanos@gmail.com>
2025-05-01 00:00:58 +0300

    created cart context

---
+++ frontend/src/contexts/CartContext.jsx
@@ -0,0 +1,46 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const CartContext = createContext();
+
+export const useCart = () => useContext(CartContext);
+
+export const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
+
+  const addItemToCart = (item) => {
+    setCart((prevCart) => {
+      const existingItemIndex = prevCart.findIndex((cartItem) => cartItem.id === item.id);
+      if (existingItemIndex > -1) {
+        const updatedCart = [...prevCart];
+        updatedCart[existingItemIndex].quantity += 1;
+        return updatedCart;
+      }
+      return [...prevCart, { ...item, quantity: 1 }];
+    });
+  };
+
+  const removeItemFromCart = (itemId) => {
+    setCart((prevCart) => prevCart.filter((item) => item.id !== itemId));
+  };
+
+  const updateItemQuantity = (itemId, quantity) => {
+    setCart((prevCart) => {
+      const updatedCart = [...prevCart];
+      const itemIndex = updatedCart.findIndex((item) => item.id === itemId);
+      if (itemIndex > -1) {
+        updatedCart[itemIndex].quantity = quantity;
+      }
+      return updatedCart;
+    });
+  };
+
+  const clearCart = () => {
+    setCart([]);
+  };
+
+  return (
+    <CartContext.Provider value={{ cart, addItemToCart, removeItemFromCart, updateItemQuantity, clearCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
